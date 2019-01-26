fcd8758a4c17e804015623cb62699a4503550e31
eric sun <ericsgy@163.com>
2019-01-26 16:40:52 +0800

    update doc

--- api_project.js
+++ api_project.js
@@ -14,7 +14,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-01-26T08:34:07.771Z",
+    "time": "2019-01-26T08:40:52.683Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
